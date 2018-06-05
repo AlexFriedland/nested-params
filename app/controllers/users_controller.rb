d8537d098e6d0547a8f0249c6073615e712cc9e5
AlexFriedland <friedlandaj@gmail.com>
2018-06-05 16:58:22 +0200

    routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,21 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def create
+    raise params.inspect
+    @user.create!(user_params)
+    redirect_to @user
+  end
+
+  private
+
+  def house_params
+    params.require(:user).permit(:email)
+  end
+
 end

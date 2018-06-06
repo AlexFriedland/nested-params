db2ad53e1fef00542f67ebddc66876e38a410b1f
AlexFriedland <friedlandaj@gmail.com>
2018-06-06 10:26:54 +0200

    done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
   has_many :addresses
   belongs_to :team
+
+  def addresses_attributes=(addresses_attributes)
+    addresses_attributes.each do |address_attributes|
+      #create address for user w these attributes
+      self.addresses.build(address_attributes)
+    end
+  end
 end
